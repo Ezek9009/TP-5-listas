44fb2257eec47ef84419e5dad4163f1a75ad69c3
Ezequiel <eze.xray@gmail.com>
2025-04-23 20:16:05 -0300

    TP-5 Listas ejercicio 2

---
+++ 2.py
@@ -0,0 +1,12 @@
+"""
+2) Crear una lista con cinco elementos 
+(colocar los elementos que más te gusten) y mostrar el 
+penúltimo. ¡Puedes hacerlo como se muestra 
+en los videos o bien investigar cómo funciona el 
+indexing con números negativos!
+"""
+
+#se crea una lista con 5 elementos
+mi_lista = ("hola mundo", 1, 2.0, True, False)
+
+print(mi_lista[3])
