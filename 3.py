2a4b463a6c791902813587ce3fb7377261eee94f
Ezequiel <eze.xray@gmail.com>
2025-04-23 21:07:30 -0300

    TP-5 Listas ejercicio 3

---
+++ 3.py
@@ -0,0 +1,19 @@
+"""
+3) Crear una lista vacía, agregar tres palabras 
+con append e imprimir la lista resultante por 
+pantalla. Pista: para crear una lista vacía
+ debes colocar los corchetes sin nada en su interior.
+Por ejemplo: 
+lista_vacia = []
+"""
+
+#lista vacia
+mi_lista = []
+
+#agrego 3 palabras a la lista
+mi_lista.append("estoy")
+mi_lista.append("agregando")
+mi_lista.append("3 palabras")
+
+#muestra en pantalla mi_lista
+print(mi_lista)
