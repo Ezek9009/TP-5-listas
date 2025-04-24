151aa023eeed64aa2c4a1147218720bfce2774df
Ezequiel <eze.xray@gmail.com>
2025-04-24 15:46:05 -0300

    TP-5 Listas ejercicio 8

---
+++ 8.py
@@ -0,0 +1,15 @@
+"""
+8) Crear una lista vacía llamada "dobles"
+ y agregar el doble de 5, 10 y 15 usando append 
+directamente. Imprimir la lista resultante por pantalla.
+"""
+#se crea una lista vacia llamada "dobles"
+dobles = []
+
+#agregamos el doble de 5, 10, 15
+dobles.append(10)
+dobles.append(20)
+dobles.append(30)
+
+#mostramos en pantalla la lista resultante
+print(dobles)
