3658e3d9348527826a19ff05c05c1b4993d1c383
Ezequiel <eze.xray@gmail.com>
2025-04-24 15:39:10 -0300

    TP-5 Listas ejercicio 7

---
+++ 7.py
@@ -0,0 +1,15 @@
+"""
+7) Reemplazar los dos valores centrales (índices 1 y 2)
+ de la lista “autos” por dos nuevos valores 
+cualesquiera. 
+autos = ["sedan", "polo", "suran", "gol"] 
+"""
+
+autos = ["sedan", "polo", "suran", "gol"]
+
+#se modifica el subindice 1 y 2, por hola! mundo!
+autos[1] = "hola!"
+autos[2] = "mundo!"
+
+#se muestra en pantalla la lista
+print(autos)
